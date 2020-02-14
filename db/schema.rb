52f15d029469b636493eb7ac6472db46ceba9bbc
John Sanders <sande303@gmail.com>
2020-02-13 22:42:50 -0800

    create Posts that belong to user; user can create/edit/delete and view a list of their posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_050331) do
+ActiveRecord::Schema.define(version: 2020_02_14_053558) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.string "category"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
